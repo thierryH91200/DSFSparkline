344eea2d295d40c5026756d7c6999c9425f2070d
thierry <thierry.hentic@gmail.com>
2021-07-01 18:11:35 +0200

    Initial Commit

---
+++ DSFSparkline/DSFSparkline.h
@@ -0,0 +1,18 @@
+//
+//  DSFSparkline.h
+//  DSFSparkline
+//
+//  Created by thierryH24 on 01/07/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DSFSparkline.
+FOUNDATION_EXPORT double DSFSparklineVersionNumber;
+
+//! Project version string for DSFSparkline.
+FOUNDATION_EXPORT const unsigned char DSFSparklineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DSFSparkline/PublicHeader.h>
+
+
